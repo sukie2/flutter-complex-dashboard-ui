9d47213d0ada46b2cc34b022fa000dad40510a9b
Sukie2 <sukie2@gmail.com>
2021-07-14 22:21:11 +0800

    Added tab bar hiding animation

--- lib/screens/dashoboard_screen.dart
+++ lib/screens/dashoboard_screen.dart
@@ -5,9 +5,37 @@ import 'package:dashboard_ui_app/widgets/floating_tab_bar.dart';
 import 'package:dashboard_ui_app/widgets/profile_section.dart';
 import 'package:dashboard_ui_app/widgets/spend_section.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
-class DashboardScreen extends StatelessWidget {
-  const DashboardScreen({Key? key}) : super(key: key);
+class DashboardScreen extends StatefulWidget {
+  @override
+  _DashboardScreenState createState() => _DashboardScreenState();
+}
+
+class _DashboardScreenState extends State<DashboardScreen> {
+  late ScrollController _controller;
+  double iconContainerHeight = 70;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = ScrollController()..addListener(_scrollListener);
+  }
+
+  void _scrollListener() {
+    if (_controller.position.userScrollDirection == ScrollDirection.reverse) {
+      if (iconContainerHeight != 0)
+        setState(() {
+          iconContainerHeight = 0;
+        });
+    }
+    if (_controller.position.userScrollDirection == ScrollDirection.forward) {
+      if (iconContainerHeight == 0)
+        setState(() {
+          iconContainerHeight = 70;
+        });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,6 +43,7 @@ class DashboardScreen extends StatelessWidget {
       alignment: Alignment.bottomCenter,
       children: [
         CustomScrollView(
+          controller: _controller,
           slivers: <Widget>[
             SliverToBoxAdapter(
               child: ProfileSection(),
@@ -38,7 +67,12 @@ class DashboardScreen extends StatelessWidget {
             ),
           ],
         ),
-        Padding(padding: EdgeInsets.only(bottom: 60), child: FloatingTabBar())
+        AnimatedContainer(
+          duration: Duration(milliseconds: 200),
+          height: iconContainerHeight,
+          padding: EdgeInsets.only(left: 16.0, right: 16.0, bottom: 16.0),
+          child: FloatingTabBar(),
+        )
       ],
     );
   }
--- lib/widgets/floating_tab_bar.dart
+++ lib/widgets/floating_tab_bar.dart
@@ -5,40 +5,35 @@ class FloatingTabBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.all(16.0),
-      child: Container(
-        height: 50.0,
-        child: Card(
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(50),
+    return Card(
+      color: Colors.white,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(50),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          Icon(
+            Icons.upload,
+            color: Colors.grey,
           ),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              Icon(
-                Icons.upload,
-                color: Colors.grey,
-              ),
-              Icon(
-                Icons.alt_route_rounded,
-                color: Colors.grey,
-              ),
-              Icon(
-                Icons.home,
-                color: Colors.pink,
-              ),
-              Icon(
-                Icons.wallet_giftcard,
-                color: Colors.grey,
-              ),
-              Icon(
-                Icons.menu,
-                color: Colors.grey,
-              ),
-            ],
+          Icon(
+            Icons.alt_route_rounded,
+            color: Colors.grey,
           ),
-        ),
+          Icon(
+            Icons.home,
+            color: Colors.pink,
+          ),
+          Icon(
+            Icons.wallet_giftcard,
+            color: Colors.grey,
+          ),
+          Icon(
+            Icons.menu,
+            color: Colors.grey,
+          ),
+        ],
       ),
     );
   }
